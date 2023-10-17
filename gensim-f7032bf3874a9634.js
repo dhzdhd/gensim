1beb1e81e682334c8b40ccdaa20b1e677f97ac63
dhzdhd <72333092+dhzdhd@users.noreply.github.com>
2023-10-17 13:07:07 +0000

    Deploying to gh-pages from @ dhzdhd/gensim@032c54f250269fa7a9d7283420ba1b387ddee5b2 🚀

--- gensim-f7032bf3874a9634.js
+++ gensim-f7032bf3874a9634.js
@@ -1903,7 +1903,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('gensim-4573d5b896545cd9_bg.wasm', import.meta.url);
+        input = new URL('gensim-f7032bf3874a9634_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
